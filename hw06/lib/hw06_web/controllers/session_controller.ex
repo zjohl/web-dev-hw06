33d6e5061cf977fd6bf137537e390a62190be171
Zamir Johl <zjohl@pivotal.io>
2018-10-17 09:13:21 -0400

    Redirect to task index after login

--- hw06/lib/hw06_web/controllers/session_controller.ex
+++ hw06/lib/hw06_web/controllers/session_controller.ex
@@ -8,11 +8,11 @@ defmodule Hw06Web.SessionController do
       conn
       |> put_session(:user_id, user.id)
       |> put_flash(:info, "Welcome back #{user.email}")
-      |> redirect(to: Routes.page_path(conn, :index))
+      |> redirect(to: Routes.task_path(conn, :index))
     else
       conn
       |> put_flash(:error, "Login failed.")
-      |> redirect(to: Routes.page_path(conn, :index))
+      |> redirect(to: Routes.task_path(conn, :index))
     end
   end
 
@@ -20,6 +20,6 @@ defmodule Hw06Web.SessionController do
     conn
     |> delete_session(:user_id)
     |> put_flash(:info, "Logged out.")
-    |> redirect(to: Routes.page_path(conn, :index))
+    |> redirect(to: Routes.task_path(conn, :index))
   end
 end
